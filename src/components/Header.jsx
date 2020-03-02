5fca09d181cabfad6feeff096d656769b8fc764f
Romain Tello <romain.tello@gmail.com>
2020-03-02 16:39:56 +0100

    Add navbar links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, NavbarBrand } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () =>
   <header>
@@ -8,8 +9,9 @@ const Header = () =>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Navbar.Text><Link to="/">Home</Link></Navbar.Text>
+          <Navbar.Text><Link to="/contact">Contact</Link></Navbar.Text>
+          <Navbar.Text><Link to="/pouet">404</Link></Navbar.Text>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
