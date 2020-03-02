97d69b81ad75a4b8420ad8cf77d564db79a65743
Romain Tello <romain.tello@gmail.com>
2020-03-02 16:22:00 +0100

    Add NotFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout } from '../components';
+
+const Home = (props) =>
+  <Layout>
+    <h1>Erreur 404</h1>
+    <p>Cette page n'existe pas (encore)...</p>
+  </Layout>
+;
+
+export default Home;
