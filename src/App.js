298f921a927129d769b31304d84ceb16189f1218
Romain Tello <romain.tello@gmail.com>
2020-03-02 12:19:20 +0100

    Add simple components with props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Card } from 'react-bootstrap';
 import './App.css';
+
+const Title = (props) =>
+  <h1 className={`title ` + props.colorName}>
+    {props.children}
+  </h1>
+;
+
+const Subtitle = (props) =>
+  <div>{props.children}</div>
+;
+
+const MyCard = (props) =>
+  <Card>
+    <Card.Body>
+      <Card.Title>{props.title || "Titre de carte"}</Card.Title>
+      <Card.Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut leo in augue vulputate laoreet. Nullam quam ligula, elementum id sodales sit amet, tempus in neque. Vestibulum et facilisis nulla. Aenean eget tellus sapien. Morbi eget libero convallis, pretium nibh at, aliquet magna. Cras in tincidunt est. Pellentesque condimentum diam lacus. Etiam viverra lacus at tellus congue, sit amet bibendum lectus egestas. Mauris lacus ligula, varius vitae lorem eu, eleifend viverra mi. Vestibulum lobortis laoreet libero, in facilisis ipsum laoreet id. Sed luctus volutpat placerat. Pellentesque luctus porta velit vel malesuada. Nullam at velit vehicula, semper est ut, ornare odio. Etiam urna massa, congue ac mauris eu, viverra vulputate lectus. Nulla venenatis sit amet orci a ultrices.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Title colorName="red">
+        Bonjour DWWM Lyon!
+      </Title>
+      <Subtitle>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Comment allez-vous les <span>amis</span>?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </Subtitle>
+
+      <div class="card-list">
+        <MyCard title="Bonjour les amis!" />
+        <MyCard title="J'aime les frites" />
+        <MyCard title="Il fait pas très beau dehors" />
+        <MyCard title="Quand est-ce qu'on mange?" />
+        <MyCard />
+      </div>
     </div>
   );
 }
