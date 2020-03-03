fef60d11c3d122e5146c01f2e6e50a44f4dde029
thothdragon <thothdragons@gmail.com>
2020-03-03 12:26:40 +0100

    Debugg selectedArticles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import PropsTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import { ArticlePreview } from '.';
-import { articles } from '../data';
 
-const ArticleList = () =>
+const ArticleList = (props) =>
   <section className="card-list">
-    {articles.map((item, index) =>
+    {props.articles.map((item, index) =>
       <ArticlePreview
         key={`article-preview${index}`}
         title={item.title}
