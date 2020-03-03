09905d54c89a7febdc8e64a4fe83f34444934a6c
thothdragon <thothdragons@gmail.com>
2020-03-03 10:26:30 +0100

    Create the category list in the sidenav

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ArticlePreview } from '.';
-import data from '../data';
+import { articles } from '../data';
 
 const ArticleList = () =>
   <section className="card-list">
-    {data.map( (item, index) =>
+    {articles.map((item, index) =>
       <ArticlePreview
         key={`article-preview${index}`}
         title={item.title}
@@ -12,6 +12,6 @@ const ArticleList = () =>
       />
     )}
   </section>
-;
+  ;
 
 export default ArticleList;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
-import data from '../data';
+import { articles, categories } from '../data';
 
 const Sidebar = () =>
   <aside>
+    <h2>Articles</h2>
     <ListGroup>
-      {data.map( (item, index) =>
+      {articles.map((item, index) =>
         <ListGroup.Item key={`article-link${index}`}>{item.title}</ListGroup.Item>
       )}
     </ListGroup>
+    <h2>Categories</h2>
+    <ListGroup>
+      {categories.map((item, index) =>
+        <ListGroup.Item key={`article-link${index}`}>{item.name}</ListGroup.Item>
+      )}
+    </ListGroup>
   </aside>
-;
+  ;
 
 export default Sidebar;
