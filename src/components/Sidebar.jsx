cdcb9a582b028654faf6400aea0f3b850a70cae1
thothdragon <thothdragons@gmail.com>
2020-03-03 10:58:43 +0100

    Refactoring Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { articles, categories } from '../data';
 
+const SidebarSection = (props) =>
+  <section>
+    <h2>{props.title}</h2>
+    <ListGroup>
+      {props.items.map((item, index) =>
+        <ListGroup.Item key={`link${index}`}>
+          {item}
+        </ListGroup.Item>
+      )}
+    </ListGroup>
+  </section>
+  ;
+
+
+
 const Sidebar = () =>
   <aside>
-    <h2>Articles</h2>
-    <ListGroup>
-      {articles.map((item, index) =>
-        <ListGroup.Item key={`article-link${index}`}>{item.title}</ListGroup.Item>
-      )}
-    </ListGroup>
-    <h2>Categories</h2>
-    <ListGroup>
-      {categories.map((item, index) =>
-        <ListGroup.Item key={`article-link${index}`}>{item.name}</ListGroup.Item>
-      )}
-    </ListGroup>
+    <SidebarSection
+      title="Articles"
+      items={articles.map((item) => item.title)}
+    />
+    <SidebarSection
+      title="Categories"
+      items={categories.map((item) => item.name)}
+    />
   </aside>
   ;
 
